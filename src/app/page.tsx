40209a30029cace8858301f0046216bfaa2cff2b
Mike7704 <Michael.cowley2001@gmail.com>
2025-01-20 21:51:25 +0000

    Added a form with some styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,51 @@
+"use client";
+
+import Image from "next/image";
 import Card from "@/components/Card";
+import CardInputForm from "@/components/CardInputForm";
 
 export default function Home() {
+  async function handleFormInput() {
+    console.log("Test");
+  }
+
   return (
-    <div className="min-h-screen bg-cover bg-center" style={{ backgroundImage: "url('/images/background.png')" }}>
-      <main className="">
-        <Card
-          name="Name"
-          quote="Quote"
-          faction="Monsters"
-          type="Hero"
-          strength="10"
-          range="Melee"
-          ability="Morale"
-          image="/images/ImageNotFound.png"
-        />
+    <div
+      className="min-h-screen flex flex-col bg-cover bg-center"
+      style={{ backgroundImage: "url('/images/background.png')" }}
+    >
+      <header className="w-full p-4 flex justify-center">
+        <Image src={"/images/logo.png"} alt={`logo`} width={321} height={210} />
+      </header>
+      <main className="container mx-auto p-4 flex-grow flex">
+        <div className="w-1/2 p-4">
+          <Card
+            name="Name"
+            quote="Quote"
+            faction="Monsters"
+            type="Hero"
+            strength="10"
+            range="Melee"
+            ability="Morale"
+            image="/images/ImageNotFound.png"
+          />
+        </div>
+        <div className="w-1/2 p-4">
+          <CardInputForm
+            name=""
+            quote=""
+            strength=""
+            faction=""
+            type=""
+            range=""
+            ability=""
+            image=""
+            onInputChange={handleFormInput}
+            onImageChange={handleFormInput}
+          />
+        </div>
       </main>
-      <footer className="text-white">
+      <footer className="w-full p-4 text-center text-white">
         <p>Michael Cowley</p>
       </footer>
     </div>
---
+++ src/components/CardInputForm.tsx
@@ -0,0 +1,160 @@
+import React from "react";
+
+interface CardInputFormProps {
+  name: string;
+  quote: string;
+  strength: string;
+  faction: string;
+  type: string;
+  range: string;
+  ability: string;
+  image: string;
+  onInputChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => void;
+  onImageChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const CardInputForm: React.FC<CardInputFormProps> = ({
+  name,
+  quote,
+  strength,
+  faction,
+  type,
+  range,
+  ability,
+  onInputChange,
+  onImageChange,
+}) => {
+  return (
+    <form className="card-input-form flex flex-col space-y-4">
+      <div className="flex flex-col">
+        <label htmlFor="name" className="mb-2 font-semibold text-white">
+          Name
+        </label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={onInputChange}
+          className="input-field p-2 border border-gray-300 rounded"
+        />
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="quote" className="mb-2 font-semibold text-white">
+          Quote
+        </label>
+        <textarea
+          name="quote"
+          value={quote}
+          onChange={onInputChange}
+          className="input-field p-2 border border-gray-300 rounded"
+        ></textarea>
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="strength" className="mb-2 font-semibold text-white">
+          Strength
+        </label>
+        <input
+          type="number"
+          name="strength"
+          value={strength}
+          onChange={onInputChange}
+          className="input-field p-2 border border-gray-300 rounded"
+        />
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="faction" className="mb-2 font-semibold text-white">
+          Faction
+        </label>
+        <select
+          name="faction"
+          value={faction}
+          onChange={onInputChange}
+          className="input-field p-2 border border-gray-300 rounded"
+        >
+          <option value="Neutral">Neutral</option>
+          <option value="Northern Realms">Northern Realms</option>
+          <option value="Nilfgaard">Nilfgaard</option>
+          <option value="SociaTael">Socia&apos;Tael</option>
+          <option value="Monsters">Monsters</option>
+          <option value="Skellige">Skellige</option>
+        </select>
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="type" className="mb-2 font-semibold text-white">
+          Type
+        </label>
+        <select
+          name="type"
+          value={type}
+          onChange={onInputChange}
+          className="input-field p-2 border border-gray-300 rounded"
+        >
+          <option value="Standard">Standard</option>
+          <option value="Hero">Hero</option>
+          <option value="Special">Special</option>
+          <option value="Leader">Leader</option>
+        </select>
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="range" className="mb-2 font-semibold text-white">
+          Range
+        </label>
+        <select
+          name="range"
+          value={range}
+          onChange={onInputChange}
+          className="input-field p-2 border border-gray-300 rounded"
+        >
+          <option value="None">None</option>
+          <option value="Melee">Melee</option>
+          <option value="Agile">Agile</option>
+          <option value="Ranged">Ranged</option>
+          <option value="Siege">Siege</option>
+        </select>
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="ability" className="mb-2 font-semibold text-white">
+          Ability
+        </label>
+        <select
+          name="ability"
+          value={ability}
+          onChange={onInputChange}
+          className="input-field p-2 border border-gray-300 rounded"
+        >
+          <option value="None">None</option>
+          <option value="Avenger">Avenger</option>
+          <option value="Bond">Bond</option>
+          <option value="Horn">Horn</option>
+          <option value="Mardroeme">Mardroeme</option>
+          <option value="Medic">Medic</option>
+          <option value="Morale">Morale</option>
+          <option value="Morph">Morph</option>
+          <option value="Muster">Muster</option>
+          <option value="Scorch">Scorch</option>
+          <option value="ScorchRow">Scorch Row</option>
+          <option value="Spy">Spy</option>
+          <option value="Clear">Clear</option>
+          <option value="Frost">Frost</option>
+          <option value="Fog">Fog</option>
+          <option value="Rain">Rain</option>
+          <option value="Storm">Storm</option>
+          <option value="Decoy">Decoy</option>
+        </select>
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="image" className="mb-2 font-semibold text-white">
+          Upload Image
+        </label>
+        <input
+          type="file"
+          name="image"
+          onChange={onImageChange}
+          className="input-field p-2 border border-gray-300 rounded"
+        />
+      </div>
+    </form>
+  );
+};
+
+export default CardInputForm;
