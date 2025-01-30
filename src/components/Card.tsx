56a766f8da7479f4c8013e70a4645a19d0db54da
Mike7704 <Michael.cowley2001@gmail.com>
2025-01-30 20:46:01 +0000

    Added more conditions for displaying card components in the correct place.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -38,39 +38,88 @@ const Card: React.FC<CardProps> = ({ name, quote, faction, type, strength, range
     ) : null;
   };
 
-  return (
-    <div
-      className="relative w-[370px] h-[575px] bg-cover bg-center text-white p-4 rounded-lg shadow-lg"
-      style={{ backgroundImage: `url(${cardImage})` }}
-    >
-      {faction === "Neutral"
-        ? getImage(typePanelNoBanner, type, 370, 128, "absolute bottom-0 left-0")
-        : getImage(typePanelBanner, type, 370, 128, "absolute bottom-0 left-0")}
-      {faction !== "Neutral" &&
-        getImage(factionBanner, faction, 80, 575, "absolute top-0 left-[7px] object-cover h-[575px]")}
-      {type === "Standard"
-        ? getImage(factionBorders, faction, 370, 575, "absolute top-0 left-0 h-[575px]")
-        : getImage(typeBorders, type, 370, 575, "absolute top-0 left-0 h-[575px]")}
-      {(type === "Standard" || type === "Hero") && getImage(typeStrengthIcon, type, 152, 152, "absolute top-0 left-0")}
-      {(type === "Standard" || type === "Hero") && getImage(rangeIcons, range, 103, 115, "absolute top-[120px] left-0")}
-      {getImage(abilityIcons, ability, 103, 115, "absolute top-[220px] left-0")}
-      {(type === "Standard" || type === "Hero") && (
+  const renderStrength = () => {
+    if (type === "Standard" || type === "Hero") {
+      return (
         <p
-          className={`absolute top-[18px] left-[52px] transform -translate-x-1/2 text-center font-calibri text-[50px] ${
+          className={`absolute top-[21px] left-[52px] transform -translate-x-1/2 text-center font-calibri text-[45px] font-semibold ${
             type === "Hero" ? "text-white" : "text-black"
           }`}
         >
           {strength}
         </p>
-      )}
+      );
+    }
+    return null;
+  };
+
+  const renderAbilityIcon = () => {
+    return getImage(
+      abilityIcons,
+      ability,
+      103,
+      115,
+      `absolute ${type === "Standard" || type === "Hero" ? "top-[220px] left-0" : "top-0 left-0"}`
+    );
+  };
+
+  const renderRangeIcon = () => {
+    if (type === "Standard" || type === "Hero") {
+      return getImage(rangeIcons, range, 103, 115, "absolute top-[120px] left-0");
+    }
+    return null;
+  };
+
+  const renderTypeStrengthIcon = () => {
+    if (type === "Standard" || type === "Hero") {
+      return getImage(typeStrengthIcon, type, 152, 152, "absolute top-0 left-0");
+    }
+    return null;
+  };
+
+  const renderFactionBanner = () => {
+    if (faction !== "Neutral") {
+      return getImage(factionBanner, faction, 80, 575, "absolute top-0 left-[7px] object-cover h-[575px]");
+    }
+    return null;
+  };
+
+  const renderTypePanel = () => {
+    return faction === "Neutral"
+      ? getImage(typePanelNoBanner, type, 370, 128, "absolute bottom-0 left-0")
+      : getImage(typePanelBanner, type, 370, 128, "absolute bottom-0 left-0");
+  };
+
+  const renderBorders = () => {
+    return type === "Standard"
+      ? getImage(factionBorders, faction, 370, 575, "absolute top-0 left-0 h-[575px]")
+      : getImage(typeBorders, type, 370, 575, "absolute top-0 left-0 h-[575px]");
+  };
+
+  return (
+    <div
+      className="relative w-[370px] h-[575px] bg-cover bg-center text-white p-4 rounded-lg shadow-lg"
+      style={{ backgroundImage: `url(${cardImage})` }}
+    >
+      {renderTypePanel()}
+      {renderFactionBanner()}
+      {renderBorders()}
+      {renderTypeStrengthIcon()}
+      {renderRangeIcon()}
+      {renderAbilityIcon()}
+      {renderStrength()}
       <p
-        className="absolute top-[456px] left-0 w-full text-center text-black font-calibri text-lg font-semibold"
+        className={`absolute top-[456px] left-0 w-full text-center text-black font-calibri text-lg font-semibold ${
+          faction !== "Neutral" ? "pl-14" : ""
+        }`}
         style={{ textShadow: "3px 1px 1px white" }}
       >
         {name}
       </p>
       <p
-        className="absolute top-[490px] left-0 px-[10px] w-full text-center text-black font-calibri italic leading-tight"
+        className={`absolute top-[490px] left-0 px-[10px] w-full text-center text-black font-calibri italic leading-tight ${
+          faction !== "Neutral" ? "pl-16" : ""
+        }`}
         style={{ textShadow: "3px 1px 1px white" }}
       >
         &quot;{quote}&quot;
